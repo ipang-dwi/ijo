c09ccf98536beaefc42d60db462135a383af1159
ipang-dwi <admin@firstplato.com>
2020-01-21 22:53:19 +0700

    update

--- ijo.bat
+++ ijo.bat
@@ -1,8 +1,8 @@
 git pull origin master
 @echo off
 @echo %DATE% >> ijo.html
-@echo "</br>" >> ijo.html
+@echo </br> >> ijo.html
 @echo %TIME% >> ijo.html
-@echo "</br>" >> ijo.html
+@echo </br> >> ijo.html
 git commit * -m "update"
 git push origin master
